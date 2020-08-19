9edbef891eebdc2c1320f404983275318e42afdd
AdamManselll <adammansellhmb@gmail.com>
2020-08-19 16:25:28 -0700

    Create first code segment for first test.
    Co-authored-by: Alma Frankenstein <a.q.frankenstein@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,42 @@
 // ---------------------- Specs -------------------------
 // Spec 1 
-Describe: `vowBegin()`
-Test: "It will add 'way' to the end of words that begin with a vowel"
-Expect(vowBegin("octopus")).toEqual("octopusway");
+// Describe: `vowBegin()`
+// Test: "It will add 'way' to the end of words that begin with a vowel"
+// Expect (vowBegin("octopus")).toEqual("octopusway");
+
 
 // Spec 2
 Describe: `consInRow()`
 Test: "For words beginning with multiple consonants in a row, it will move all of them to the end of the word and add an 'ay'."
-Expect(consInRow("streetlight")).toEqual("eetlightstray");
+// Expect: (consInRow("streetlight")).toEqual("eetlightstray");
 
 // Spec 3
 Describe: `quMove()`
 Test: "If the first consonants include,'qu', the program moves the 'u' along with the 'q'."
-Expect(pigLatin("squeal")).toEqual("ealsquay");
+// Expect: (pigLatin("squeal")).toEqual("ealsquay");
 
 // Spec 4
 Describe: `yCons()`
 Test: "For words beginning with 'y', the program treats 'y' as a consonant."
-Expect(pigLatin("yum")).toEqual("umyay");
+// Expect: (pigLatin("yum")).toEqual("umyay");
+
+
+// ----------------------- Tests --------------------------
+// Test 1
+
+// Code to Test
+
+$(document).ready(function () {
+  $("#pigForm").submit(function (event) {
+    function vowBegin(sentence) {
+      if (/[aeiou]/.test(sentence)) {
+        sentence += "way";
+        return sentence;
+      } else {
+        return sentence;
+      }
+    }
+    console.log(vowBegin("octopus"));
+    event.preventDefault();
+  });
+});
